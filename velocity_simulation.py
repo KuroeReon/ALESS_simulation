c2e66c1d901678e4b368f57704f498df2638a581
KeitaIHARA <keita18060808@gmail.com>
2025-10-31 17:55:44 +0900

    さらなる改善

--- velocity_simulation.py
+++ velocity_simulation.py
@@ -1,12 +1,18 @@
 from derivative_veocity import Velocity, derivative_velocity
 from Runge-Kutta import runge_kutta
+from get_initial_cartesian_velocity import get_initial_velocity
 
-def run_simulation(config, initial_state, t, dt, max_t, states, thrust, mass):
-    state = initial_state
-    states.append(state)
-    while t < max_t and state.height >= 0:
-        state = runge_kutta(derivative, states[-1], config, t, dt, mass, thrust)
-        states.append(state)
+initial_velocity = get_initial_velocity
+
+t = 100
+dt = 0.01
+
+def run_velocity_simulation(initial_velocity, t, dt, max_t, derivative_velocity):
+    velocities = []
+    velocity = initial_velocity
+    velocities.append(velocity)
+    while t < max_t:
+        velocity = runge_kutta(derivative_velocity, velocities[-1], t, dt)
+        velocities.append(velocity)
         t += dt
-
-main()
+    return velocities
